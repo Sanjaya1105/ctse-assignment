a9e0660c3a2592d8fb916b18a042f56bd7043f95
Sanjaya Jayasinghe <njaya100@gmail.com>
2026-03-22 12:53:55 +0530

    add chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -906,7 +906,7 @@ export default function App() {
 
   return (
     <div className="page">
-      <h1>CTSE Microservices via API Gateway tesssssting 22211</h1>
+      <h1>CTSE Microservices via API Gateway tesssssting 22211 333</h1>
       <p>
         API base:{' '}
         <code>
